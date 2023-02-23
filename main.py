fa2a2f8b16ffce3e1791cdee1c299c4f19fa4b94
ecriminals <120739758+ecriminals@users.noreply.github.com>
2023-02-23 11:30:28 -0500

    fixed error in my own comment.💀

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 
 
 class Hashes:
-    # honestly i'm pretty sure stag is a md4 hash and md5, but md4 won't work so I did md5.
+    # honestly i'm pretty sure stag is a md4 hash and not a md5, but md4 won't work so I did md5.
     def stag(username: str):
         data = f"{username}-{len(username)}-eucalyptus"
         stag = hashlib.new(
